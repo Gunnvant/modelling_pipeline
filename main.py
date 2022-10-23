6988a8e5b0a3dbcec47edc32706fd121b51b647d
Gunnvant <gunnvant3.14@gmail.com>
2022-10-23 17:16:09 +0530

    version updated in main.py

--- main.py
+++ main.py
@@ -40,7 +40,7 @@ def go(config: DictConfig):
             _ = mlflow.run(
                 f"{config['main']['components_repository']}/get_data",
                 "main",
-                version=0.0.1,
+                version="v0.0.1",
                 parameters={
                     "sample": config["etl"]["sample"],
                     "artifact_name": "sample.csv",
